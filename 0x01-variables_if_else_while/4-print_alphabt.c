a1188464e95deb4d7e3cde102f289ef5f0130a73
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-17 15:37:49 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	int s = 97, e = 122;
+
+	while (s <= e)
+	{
+		if (s == 101 || s == 113)
+		{
+			s++;
+			continue;
+		}
+		putchar(s++);
+	}
+	putchar("\n");
+	return (0);
+}
+
