703d03432df74114ae04224292d06cb002a555e1
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-17 15:52:07 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int s = 48, e = 57;
+
+	while (s <= e)
+	{
+		putchar(s++);
+		if (s == e + 1)
+			putchar('$');
+		else
+		{
+			putchar(44);
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
