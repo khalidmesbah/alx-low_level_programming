a89a67360eab7a2e2e6c13b7bae01c15131beb56
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-16 17:11:23 -0800

    up

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
 /**
  * main - Entry point
  * Description: 'the program's description'
- * Return: Always 0 (Success)
+ * Return: Always 1 (Failure)
  */
 
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	syscall(SYS_write, 1, msg, 59);
 	return (1);
 }
