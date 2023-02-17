f3a39b39b52612a48fa7d8169d6a5aed30bec889
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-17 14:31:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	int s = 97, e = 123;
+
+	while (s < e)
+	{
+		putchar(s++);
+	}
+
+	return (0);
+}
+
