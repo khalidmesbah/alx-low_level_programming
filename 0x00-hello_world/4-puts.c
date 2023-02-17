e3293fb3227825df4e178349e0f8c08fe8f720c0
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-16 16:24:19 -0800

    Up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-int main()
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
