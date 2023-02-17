f38f83f151667fc5d57711aba7de71d8f958a39c
khalidmesbah <swekhalidmesbah6@gmail.com>
2023-02-16 17:07:31 -0800

    up

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/syscall.h>
 
 /**
@@ -9,7 +9,8 @@
 
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, sizeof(msg)); 
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	syscall(SYS_write, 1, msg, sizeof(msg));
 	return (1);
 }
